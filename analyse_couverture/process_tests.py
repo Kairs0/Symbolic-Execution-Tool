00a7bfabc54e1c50ed0667af916226413e34e90e
Kairs0 <arnaud.convers@gmail.com>
2018-01-25 11:38:13 +0100

    update

--- analyse_couverture/process_tests.py
+++ analyse_couverture/process_tests.py
@@ -3,37 +3,6 @@
 
 from asttree import Node
 
-"""
-{1: ('if', '<=', [0], (2, 3)),
-2: ('assign', '0-X', '', 4),
-3: ('assign', '1-X', '', 4),
-4: ('if', '==', [1], (5, 6)),
-5: ('assign', '1', '', 7), 
-6: ('assign', 'X+1', '', 7)}
-"""
-
-
-graph_prog = {
-    1: ("if", "<=", [0], (2, 3)),
-    2: ("assign", "-x", "", 4),
-    3: ("assign", "1-x", "", 4),
-    4: ("if", "==", [1], (5, 6)),
-    5: ("assign", "1", "", 0),
-    6: ("assign", "x+1", "", 0)
-}
-
-graph_prog_test = {
-    1: ("if","<=", [0], (2, 3)), # node[0] : type (assign, while, if, skip)
-    2: ("assign", "-x", "", 4),
-    3: ("assign", "1-x", "", 4),
-    4: ("if","==", [0], (5, 6)),
-    5: ("assign", "1", "", 8),
-    6: ("assign", "x+1", "", 8),
-    7: ("skip", 9),
-    8: ("while", "<=", [4], (6, 7)),
-    9: ("assign", "x*x", "", 0)
-}
-
 # TODO: compare between x and y for if and while
 # while loop is made with a second node which points to the while node
 
@@ -150,88 +119,44 @@ def toutes_decisions(valeurs_test, graph):
         print("Nodes " + str(objective) + " were never reached.")
 
 
-def buildTree():
-    tree_prog = Node("sequence")
-
-    # Sequence 1 : if1
-    if1 = Node("if")
-
-    # condition if1
-    compare1 = Node("compare", "<=")
-    var1 = Node("variable", "X")
-    cst1 = Node("constant", 0)
-    compare1.add_child(var1)
-    compare1.add_child(cst1)
-    if1.add_child(compare1)
-
-    # if body if1
-    assign1 = Node("assign")
-    var1 = Node("variable", "X")
-    op1 = Node("operation", "-")
-    cst2 = Node("constant", 0)
-    var2 = Node("variable", "X")
-    op1.add_child(cst2)
-    op1.add_child(var2)
-    assign1.add_child(var1)
-    assign1.add_child(op1)
-    if1.add_child(assign1)
-
-    # else body if1
-    assign2 = Node("assign")
-    var3 = Node("variable", "X")
-    op2 = Node("operation", "-")
-    cst3 = Node("constant", 1)
-    var4 = Node("variable", "X")
-    op2.add_child(cst3)
-    op2.add_child(var4)
-    assign2.add_child(var3)
-    assign2.add_child(op2)
-    if1.add_child(assign2)
-
-    # end seq 1
-    tree_prog.add_child(if1)
-
-    # seq 2 : if2
-    if2 = Node("if")
-
-    # condition if2
-    compare2 = Node("compare", "==")
-    var5 = Node("variable", "X")
-    cst4 = Node("constant", 1)
-    compare2.add_child(var5)
-    compare2.add_child(cst4)
-    if2.add_child(compare2)
-
-    # if body if2
-    assign3 = Node("assign")
-    var6 = Node("variable", "X")
-    cst5 = Node("constant", 1)
-    assign3.add_child(var6)
-    assign3.add_child(cst5)
-    if2.add_child(assign3)
-
-    # else body if2
-    assign4 = Node("assign")
-    var7 = Node("variable", "X")
-    op3 = Node("operation", "+")
-    var8 = Node("variable", "X")
-    cst6 = Node("constant", 1)
-    op3.add_child(var8)
-    op3.add_child(cst6)
-    assign4.add_child(var7)
-    assign4.add_child(op3)
-
-    if2.add_child(assign4)
-
-    # end seq2
-    tree_prog.add_child(if2)
-
-    return tree_prog
-
-
 if __name__ == '__main__':
 
-    PATH_TESTS = "tests/test.txt"
+    PATH_TESTS = "tests_txt/test.txt"
+
+    ### TMP: graph générés ou écrits à la main
+    # pour process tests avant branchement convertisseut astToCfg
+
+    generated_graph_prog = {
+        1: ('if', '<=', [0], (2, 3)),
+        2: ('assign', '0-x', '', 4),
+        3: ('assign', '1-x', '', 4),
+        4: ('if', '==', [1], (5, 6)),
+        5: ('assign', '1', '', 0), 
+        6: ('assign', 'x+1', '', 0)
+    }
+
+
+    graph_prog = {
+        1: ("if", "<=", [0], (2, 3)),
+        2: ("assign", "-x", "", 4),
+        3: ("assign", "1-x", "", 4),
+        4: ("if", "==", [1], (5, 6)),
+        5: ("assign", "1", "", 0),
+        6: ("assign", "x+1", "", 0)
+    }
+
+    graph_prog_test = {
+        1: ("if","<=", [0], (2, 3)), # node[0] : type (assign, while, if, skip)
+        2: ("assign", "-x", "", 4),
+        3: ("assign", "1-x", "", 4),
+        4: ("if","==", [0], (5, 6)),
+        5: ("assign", "1", "", 8),
+        6: ("assign", "x+1", "", 8),
+        7: ("skip", 9),
+        8: ("while", "<=", [4], (6, 7)),
+        9: ("assign", "x*x", "", 0)
+    }
+
 
     valeurs_test = []
     with open(PATH_TESTS) as file:
@@ -241,6 +166,5 @@ if __name__ == '__main__':
     toutes_affectation(valeurs_test, graph_prog)
     toutes_decisions(valeurs_test, graph_prog)
 
-    ast_test = buildTree()
-
-    # ast_test.print_me()
+    toutes_affectation(valeurs_test, generated_graph_prog)
+    toutes_decisions(valeurs_test, generated_graph_prog)
