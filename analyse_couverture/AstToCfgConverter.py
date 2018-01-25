ea76ce345024da8e8d519049c55b94f2328c10ee
Kairs0 <arnaud.convers@gmail.com>
2018-01-25 16:23:27 +0100

    update doc for cfg graph

--- analyse_couverture/AstToCfgConverter.py
+++ analyse_couverture/AstToCfgConverter.py
@@ -1,38 +1,39 @@
-"""
-CFG Graph Structure:
-The graph is a dictionary.
-Key is the numeration of the node
-Value is a tuple containing instructions
-tuple[0] contains the command type:
-it can be "assign", "while", "if" or "skip"
+# -*- coding: utf-8 -*-
+"""CFG Graph Structure:
 
-***** For "if" and "while" commands:
-tuple[1] is the comparator it can be "<=" to ">="
-tuple[2] is a list of values used to operate the
-operator given in tuple[1]. It's a length 2 list, with
-list[0] the first value to be compared,
-and list[1] the second value
-these two can either be a string ("x" or "y") or an int
+The CFG Graph is stored as a dictionary.
+    Key is the number of the node
+    Value is a tuple containing information about the node:
 
-****** ******* TODO add ability to compare y without comparing x
-If the list is length 2, the first value will be checked 
-against the second
-tuple[3] contains a tuple of 2 elements.
-The first one is the following node when the
-statement is true, the second one the following node when the 
-statement is false.
+    Value[0] contains the command type:
+    it can be "assign", "while", "if" or "skip"
 
-***** For "assign" commands:
-tuple[1] is the new value of the first variable x
-it reads like : x = tuple[1]
-tuple[2] is the new value of the second variable y
-it reads like y = tuple[2]
-tuple[3] is the following node
+    ***** "if" and "while" commands:
+        Value[1] is the comparator. It can be "<=", ..., ">="
 
-***** For "skip" commands:
-tuple[1] is the following node
+        Value[2] is a length 2 list of values on which the Value[1] operates.
+            list[0] is the first value to be compared,
+            list[1] the second value
+            These two values can either be a string ("x" or "y") or an int
 
-Example graph for "prog" program:
+        Value[3] contains a tuple of 2 integer.
+        The first one is the following node when the statement is true,
+        the second one the following node when the statement is false.
+
+    ***** "assign" commands:
+        Value[1] is the new value of the first variable x
+        it reads like : x = tuple[1]
+        Value[2] is the new value of the second variable y
+        it reads like y = tuple[2]
+        Value[3] is the number of the following node
+
+    ***** For "skip" commands:
+        Value[1] is the number of the following node
+
+We can see that the ultimate value of the value is always one or more following node.
+Value zero represents the _ sign (absence of following node)
+
+An Example: graph for "prog" program:
 graph_prog = {
     1: ("if", "<=", ["x", 0], (2, 3)),
     2: ("assign", "-x", "", 4),
