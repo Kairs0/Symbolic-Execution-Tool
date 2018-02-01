a87fe361fb694c0d9c56ff94553b833da9771b95
Kairs0 <arnaud.convers@gmail.com>
2018-02-01 21:17:30 +0100

    litlle work on parser... to be continued

--- parser.py
+++ parser.py
@@ -1,14 +1,16 @@
 booleans = ["true", "false"]
 operators = ['+', '-', '*']
-comparators = ['==', '<=', '<', '>', '>=']
-keywords = ['if', 'then', 'else', 'while', ':=']
+comparators = ['==', '<=', '<', '>', '>=', '!=']
+keywords = ['if', 'then', 'else', 'while', ':=', 'end']
+separators = ['(', ')', '{', '}', ';']
+numbers = [x for x in range(10)]
 
 
 def tokenize(program_lines_as_list):
     result = []
     for line in program_lines_as_list:
         result = result + tokenize_line(line)
-        result.append('separator')
+        # result.append('separator')
     return result
 
 
@@ -24,10 +26,24 @@ def tokenize_line(program_line):
             result.append(['comparator', word])
         elif word in keywords:
             result.append(['keyword', word])
+        elif word.isnumeric():
+            result.append(['numeric', word])
+        elif word in separators:
+            result.append(['separator', word])
         else:
             result.append(['identifier', word])
-
     return result
+
+
+def build_blocks(tokens):
+    # TODO wip
+    blocks = []
+    current_block = []
+    for token in tokens:
+        if token[0] == 'while':
+            current_block.append(token)
+
+    pass
 
 
 if __name__ == "__main__":
