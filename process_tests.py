5e8d27ee4fd140652a9ce56f5a649a831e6a0960
Kairs0 <arnaud.convers@gmail.com>
2018-02-09 19:37:58 +0100

    add toutes conditions test coverage

--- process_tests.py
+++ process_tests.py
@@ -4,16 +4,21 @@
 LIMIT_FOR_INFINITE_LOOP = 100
 
 
-def process_value_test(graph, variables):
+def process_value_test(graph, variables, info_conditions=False):
     """
     :param graph: CFG graph
     :param variables: a dictionary {var: initial_value}
+    :param info_conditions: a boolean, if true: will return an additional value which is a dic,
+    stating how each condition was evaluated. (Todo)
     :return: steps the program went through, dic of final values of variables
     """
     path = []
     next_node = 1
     path.append(next_node)
     count = 0
+    if info_conditions:
+        dic_result_cond = {}
+
     while next_node != 0 and count <= LIMIT_FOR_INFINITE_LOOP:
         if count == LIMIT_FOR_INFINITE_LOOP:
             raise ValueError('Infinite loop - program stopped')
@@ -21,6 +26,10 @@ def process_value_test(graph, variables):
         node = graph[next_node]
         if node[0] == "if" or node[0] == "while":
             bool_result = process_bool_expression(node[1], variables)  # check condition, returns True or False
+
+            if info_conditions:
+                dic_result_cond[next_node] = analyze_conditions(node[1], variables)
+
             next_node = node[-1][0] if bool_result else node[-1][1]
         elif node[0] == "skip":
             next_node = node[1][0]
@@ -32,7 +41,35 @@ def process_value_test(graph, variables):
 
         path.append(next_node)
         count += 1
-    return path, variables
+
+    if info_conditions:
+        return path, variables, dic_result_cond
+    else:
+        return path, variables
+
+
+def analyze_conditions(bool_expr, variables):
+    """
+    For each condition in given boolean expression, returns the boolean evaluated.
+    The result is returned as a list.
+    :param bool_expr:
+    :param variables: dic of variables and their values
+    :return: list of booleans
+    """
+
+    conditions = get_conditions_from_bool_expression(bool_expr)
+    result = []
+    for comparison in conditions:
+        operator = comparison[0]
+        values = comparison[1]
+        result_comparison = compare(
+            operator,
+            variables[values[0]] if values[0] in variables else values[0],
+            variables[values[1]] if values[1] in variables else values[1],
+        )
+        result.append(result_comparison)
+
+    return result
 
 
 def process_bool_expression(conditions, variables):
@@ -138,10 +175,14 @@ def replace_any_var_by_value(instruction, variables):
 
 
 def get_all_conditions_from_graph(graph):
-    conditions = []
+    """
+    :param graph:
+    :return: returns a dictionary {node: list(conditions)}
+    """
+    conditions = {}
     for node, value in graph.items():
         if value[0] == 'if' or value[0] == 'while':
-            conditions.append(get_conditions_from_bool_expression(value[1]))
+            conditions[node] = get_conditions_from_bool_expression(value[1])
 
     return conditions
 
@@ -401,7 +442,37 @@ def all_definitions(values_test, graph):
         print("TDef: fails")
 
 
-# def all_ut
+def all_conditions(values_test, graph):
+    print("\n ------")
+    print("Criterion: all conditions")
+
+    # dictionary {node: list(conditions)}
+    conditions = get_all_conditions_from_graph(graph)
+
+    result_true = {str(condition): False for condition in conditions.values()}
+    result_false = result_true.copy()
+
+    # when a condition is evaluated to true in the program, we set its value to true in result_true. When it's evaluated to false, we set its value to false in result_false.
+
+    for value in values_test:
+        path, var, info_cond = process_value_test(graph, value, info_conditions=True)
+        # info cond: a dic {node: list(evaluated conditions)
+        for index_node, list_results in info_cond.items():
+            for index, result in enumerate(list_results):
+                condition_just_evaluated = conditions[index_node][index]
+                if result:
+                    result_true[str([condition_just_evaluated])] = True
+                else:
+                    result_false[str([condition_just_evaluated])] = True
+
+    if all(correct for correct in result_true.values()) and all(correct for correct in result_false.values()):
+        print("TC: OK")
+    else:
+        # print("result_true")
+        # print(result_true)
+        # print("result_false")
+        # print(result_false)
+        print("TC: fails")
 
 
 def read_test_file(path_tests):
@@ -476,3 +547,9 @@ if __name__ == '__main__':
 
     test_values = read_test_file('sets_tests_txt/tests_for_fact.txt')
     all_definitions(test_values, graph_factorial)
+
+    test_values = read_test_file('sets_tests_txt/tests_for_fact.txt')
+    all_conditions(test_values, graph_factorial)
+
+    test_values = read_test_file(PATH_TESTS)
+    all_conditions(test_values, test_two_variables)
